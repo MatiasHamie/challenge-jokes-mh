403a3e5a3bf3473f09583cb8e4b8d6d7d6bd8304
Matias Hamie <matias@bytandym.com>
2024-09-27 16:55:05 +0200

    chore: remove comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], // esto va a mano
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
